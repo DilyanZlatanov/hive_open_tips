2a81cd1484349e0f83b6fd66943c66e5a1f395c8
DilyanZlatanov <dilyanzlatanov@students.softuni.bg>
2024-04-25 17:27:58 +0300

    add endpoints

--- api/endpoints.sql
+++ api/endpoints.sql
@@ -1,57 +1,116 @@
 -- Functions creating API endpoints
 -- The function name is the endpoint name, accessible under domain.com/rpc/function_name
 
-
--- The following function provides data for every tip-like transfer on the blockchain.
-CREATE OR REPLACE FUNCTION tips()
+-- This function provides a list for tips for a specific Hive post.
+CREATE OR REPLACE FUNCTION tips_list_for_post(v_author VARCHAR, v_permlink TEXT)
 RETURNS TABLE (
-   trx_id BIGINT,
-   sender VARCHAR(16),
-   receiver VARCHAR(16),
-   amount FLOAT,
-   token VARCHAR(20),
-   created TIMESTAMP,
-   platform VARCHAR,
-   memo TEXT
-   )
-AS $$
-
-BEGIN
-RETURN QUERY
-
-SELECT 
-  t.trx_id,
-  t.sender, 
-  t.receiver, 
-  t.amount, 
-  t.token,
-  t.timestamp,
-  t.platform,
-  t.memo
-FROM hive_open_tips t
-ORDER BY trx_id ASC;
-      
-END;
-$$
-LANGUAGE plpgsql;
-
-
-CREATE OR REPLACE FUNCTION tips_sum_for_post(post TEXT)
-RETURNS TABLE (
-   total_tips FLOAT,
-   token VARCHAR(20),
-   hive_post VARCHAR(100)
+  op_id BIGINT,
+  "timestamp" TIMESTAMP,
+  amount FLOAT,
+  token VARCHAR(20),
+  sender VARCHAR(16),
+  author VARCHAR(16),
+  permlink TEXT,
+  parent_author VARCHAR(16),
+  parent_permlink TEXT,
+  memo TEXT,
+  platform VARCHAR(50)
 )
 AS $$
 BEGIN
     RETURN QUERY
     SELECT 
-      t.SUM(amount),
-      t.token,
-      t.memo
+    t.hafsql_op_id,
+    t.timestamp,
+    t.amount,
+    t.token,
+    t.sender,
+    t.author,
+    t.permlink,
+    t.parent_author,
+    t.parent_permlink,
+    t.memo,
+    t.platform
     FROM hive_open_tips t
-    WHERE memo = post
-    GROUP BY memo, token;
+    WHERE t.parent_author = '' 
+    AND t.author_permlink = $1 || '/' || $2;
+END;
+$$
+LANGUAGE plpgsql;
+
+
+
+-- This function provides list for tips for every comment on a specific Hive post.
+CREATE OR REPLACE FUNCTION tips_list_for_post_comments(v_author VARCHAR, v_permlink TEXT)
+RETURNS TABLE (
+  op_id BIGINT,
+  "timestamp" TIMESTAMP,
+  amount FLOAT,
+  token VARCHAR(20),
+  sender VARCHAR(16),
+  author VARCHAR(16),
+  permlink TEXT,
+  parent_author VARCHAR(16),
+  parent_permlink TEXT,
+  memo TEXT,
+  platform VARCHAR(50)
+)
+AS $$
+BEGIN
+    RETURN QUERY
+    SELECT
+    t.hafsql_op_id,
+    t.timestamp,
+    t.amount,
+    t.token,
+    t.sender,
+    t.author,
+    t.permlink,
+    t.parent_author,
+    t.parent_permlink,
+    t.memo,
+    t.platform
+    FROM hive_open_tips t 
+    WHERE t.parent_author != ''
+    AND t.parent_author = $1
+    AND t.parent_permlink = $2;
+END;
+$$
+LANGUAGE plpgsql;
+  
+
+-- This function accepts POST request. You can pass JSONB array as parameter based on author_permlink column from hive_open_tips table.
+CREATE OR REPLACE FUNCTION tips_for_multi_posts_selection(posts JSONB)
+RETURNS TABLE(
+  op_id BIGINT,
+  "timestamp" TIMESTAMP,
+  amount FLOAT,
+  token VARCHAR(20),
+  sender VARCHAR(16),
+  author VARCHAR(16),
+  permlink TEXT,
+  parent_author VARCHAR(16),
+  parent_permlink TEXT,
+  memo TEXT,
+  platform VARCHAR(50)
+)
+AS $$
+BEGIN
+  RETURN QUERY 
+  SELECT
+  t.hafsql_op_id,
+  t.timestamp,
+  t.amount,
+  t.token,
+  t.sender,
+  t.author,
+  t.permlink,
+  t.parent_author,
+  t.parent_permlink,
+  t.memo,
+  t.platform
+  FROM hive_open_tips t
+  WHERE t.author_permlink = ANY(SELECT jsonb_array_elements_text($1));
 END;
 $$
 LANGUAGE plpgsql;
